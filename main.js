263c321e828dde71d31af72a113ddb7f6e2f5711
Daniel Paschal <daniel.paschal@learningfuze.com>
2018-06-28 15:58:39 -0700

    added do math function for +, -, /, *, x, X

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+function doMath(op, num1, num2){
+	var opDictionary = {
+		'+': add,
+		'-': subtract,
+		'*': multiply,
+		'/': divide
+	}
+	opDictionary['X'] = opDictionary['x'] = opDictionary['*'];
+	return opDictionary[op](num1, num2);
+}
+
 function add(n1, n2){
    return n1+n2;
 }
